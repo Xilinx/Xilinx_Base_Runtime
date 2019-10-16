609a5d88cdd86ee4b3288d29ae78b5862a61882b
tianyul <tianyul@xilinx.com>
2019-10-16 14:14:49 -0700

    prune files

--- build_all.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#
-# (C) Copyright 2019, Xilinx, Inc.
-#
-#!/usr/bin/env bash
-
-cd Dockerfiles
-for path in $(find . -name build.sh )  ; do cd $(dirname "$path"); ./build.sh; cd -; done
