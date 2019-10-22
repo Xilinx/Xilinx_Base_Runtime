568cbc2d276395cc5a716a5aad5c1be3906c3fb0
tianyul <tianyul@xilinx.com>
2019-10-22 10:22:08 -0700

    reduce docker images to support runtime only

---
+++ Dockerfiles/2019.1/centos/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# (C) Copyright 2019, Xilinx, Inc.
+#
+#!/usr/bin/env bash
+
+TAG="xilinx/xilinx_runtime_base:alveo-2019-1-centos"
+
+while true
+do
+case "$1" in
+	-t|--tag        ) TAG="$2"     ; shift 2 ;;
+*) break ;;
+esac
+done
+
+docker build -t $TAG .
+
+docker push $TAG
